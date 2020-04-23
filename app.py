313a97414125eec402d0db48d6d8da0c3331e8ca
root <root@bastion.ocp4.its4u.local>
2020-04-23 16:35:17 +0200

    change option

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import random
 import json
 import requests
 
-option_a = os.getenv('OPTION_A', "BIL")
-option_b = os.getenv('OPTION_B', "ING")
+option_a = os.getenv('OPTION_A', "kubernetes")
+option_b = os.getenv('OPTION_B', "openshift")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
